b64f213c1a833b652b07808e806e36e344949ce6
ruhtar <arthur.amorim10@gmail.com>
2022-12-17 17:49:34 -0300

    Quinto commit

--- script.js
+++ script.js
@@ -18,3 +18,26 @@ const hideElements = () => {
     document.getElementById("hideable-container").style.display = "none"
   }
 }
+
+const createTextArea = (event) => {
+  event.preventDefault();
+  if(document.getElementById("readonly-textarea")){
+    document.getElementById("readonly-textarea").textContent = "Já existe";
+    showInfos();
+  }
+  else{
+    const readonlyTextArea = document.createElement("textarea")
+    readonlyTextArea.setAttribute("id", "readonly-textarea")
+    readonlyTextArea.setAttribute("readonly", "true")
+    showInfos();
+    document.getElementById("infos").appendChild(readonlyTextArea)
+  }
+}
+
+const showInfos = () => {
+  const name = document.getElementById('name').value
+  const age = document.getElementById('age').value
+  const date = document.getElementById('date').value
+  const sex =  document.getElementById('date').value
+
+}
