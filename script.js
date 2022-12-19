e343570135a088af799b2214b7fb8fb89ae39f86
ruhtar <arthur.amorim10@gmail.com>
2022-12-19 11:38:40 -0300

    Refatorando funções

--- script.js
+++ script.js
@@ -1,15 +1,3 @@
-// Seus scripts aqui
-
-/* function datetime() {
-  var title = document.getElementById('title');
-  var now = new Date();
-  
-  title.innerHTML = title.innerHTML + ' (' + now.toLocaleTimeString() + ')';
-}
-
-
-datetime(); */
-
 const hideElements = () => {
   if(document.getElementById("checkbox").checked){
     document.getElementById("hideable-container").style.display = "block"
@@ -19,31 +7,34 @@ const hideElements = () => {
   }
 }
 
+const getInfos = () => {
+  const name = document.getElementById('name').value
+  const age = document.getElementById('age').value
+  const date = document.getElementById('date').value //YYYY-MM-DD
+  const sex = document.querySelector('input[name="sexAnswer"]:checked').value
+  const formattedDate = date.split("-").reverse().join("-"); //DD-MM-YYYY
+  const number = getWordNumber() //Função que retorna o número de palavras
+  return (
+`Nome: ${name}
+Idade: ${age}
+Sexo: ${sex}
+Data: ${formattedDate}
+Número de palavras contadas: ${number}`);
+}
+
 const createAndUpdateTextArea = (event) => {
   event.preventDefault();
 /*   if(document.getElementById("readonly-textarea")){
     document.getElementById("readonly-textarea").textContent = "Já existe";
   } */
-    const readOnlyTextArea = document.createElement("textarea")
-    readOnlyTextArea.setAttribute("class", "readonly-textarea")
-    readOnlyTextArea.setAttribute("readonly", "true")
-    const name = document.getElementById('name').value
-    const age = document.getElementById('age').value
-    const date = document.getElementById('date').value
-    console.log(date);
-    const sex = document.querySelector('input[name="sexAnswer"]:checked').value
-    const textContent = document.getElementById('text-content').value
-    const number = getWordNumber(textContent)
-    const formattedDate = date.split("-").reverse().join("-");
-    readOnlyTextArea.value = 
-`Nome: ${name}
-Idade: ${age}
-Sexo: ${sex}
-Data: ${formattedDate}
-Número de palavras contadas: ${number}`
-    document.getElementById("infos").appendChild(readOnlyTextArea)
+  const readOnlyTextArea = document.createElement("textarea")
+  readOnlyTextArea.setAttribute("class", "readonly-textarea")
+  readOnlyTextArea.setAttribute("readonly", "true")
+  readOnlyTextArea.value = getInfos();
+  document.getElementById("infos").appendChild(readOnlyTextArea)
 }
- 
-const getWordNumber = (text) => {
-  return text.split(' ').filter((element) =>  element !== '' ).length;
+
+const getWordNumber = () => {
+  const textContent = document.getElementById("text-content").value
+  return textContent.split(' ').filter((element) =>  element !== '' ).length;
 }
