59bcb5ca38d6b6a56c25ee3d5e431c1d827f0a88
ruhtar <arthur.amorim10@gmail.com>
2022-12-19 22:06:07 -0300

    Projeto finalizado

--- script.js
+++ script.js
@@ -1,40 +1,48 @@
 const hideElements = () => {
   if(document.getElementById("checkbox").checked){
-    document.getElementById("hideable-container").style.display = "block"
+    document.getElementById("hideable-container").style.display = "block";
   }
   else{ 
-    document.getElementById("hideable-container").style.display = "none"
+    document.getElementById("hideable-container").style.display = "none";
   }
 }
 
 const getInfos = () => {
-  const name = document.getElementById('name').value
-  const age = document.getElementById('age').value
-  const date = document.getElementById('date').value //YYYY-MM-DD
-  const sex = document.querySelector('input[name="sexAnswer"]:checked').value
+  const name = document.getElementById('name').value;
+  const age = document.getElementById('age').value;
+  const sex = document.querySelector('input[name="sexAnswer"]:checked').value;
+  const date = document.getElementById('date').value; //YYYY-MM-DD
   const formattedDate = date.split("-").reverse().join("-"); //DD-MM-YYYY
-  const number = getWordNumber() //Função que retorna o número de palavras
+  const numberOfWords = getWordNumber();
   return (
 `Nome: ${name}
-Idade: ${age}
+Idade: ${age} anos
 Sexo: ${sex}
 Data: ${formattedDate}
-Número de palavras contadas: ${number}`);
+Número de palavras digitadas: ${numberOfWords}`);
 }
 
 const createAndUpdateTextArea = (event) => {
   event.preventDefault();
-/*   if(document.getElementById("readonly-textarea")){
-    document.getElementById("readonly-textarea").textContent = "Já existe";
-  } */
-  const readOnlyTextArea = document.createElement("textarea")
-  readOnlyTextArea.setAttribute("class", "readonly-textarea")
-  readOnlyTextArea.setAttribute("readonly", "true")
+  const readOnlyTextArea = document.createElement("textarea");
+  readOnlyTextArea.setAttribute("class", "readonly-textarea");
+  readOnlyTextArea.setAttribute("readonly", "true");
+  readOnlyTextArea.setAttribute("resize", "none");
   readOnlyTextArea.value = getInfos();
-  document.getElementById("infos").appendChild(readOnlyTextArea)
+  document.getElementById("infos").appendChild(readOnlyTextArea);
+  clearInputs();
 }
 
 const getWordNumber = () => {
-  const textContent = document.getElementById("text-content").value
+  const textContent = document.getElementById("text-content").value;
   return textContent.split(' ').filter((element) =>  element !== '' ).length;
+}
+
+const clearInputs = () => {
+  document.getElementById('name').value = '';
+  document.getElementById('age').value = '';
+  document.getElementById('date').value = '';
+  document.getElementById('Feminino').checked = false;
+  document.getElementById('Masculino').checked = false;
+  document.getElementById("text-content").value = '';
 }
